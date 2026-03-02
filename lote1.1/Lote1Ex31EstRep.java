1065efa0af505abba63f563a831728ade1ee7aba
d-caio <diascaio@proton.me>
2026-03-02 15:38:53 -0300

    Lote 1.1, Ex 31

---
+++ lote1.1/Lote1Ex31EstRep.java
@@ -0,0 +1,17 @@
+/*
+OBJETIVO: Calcule e mostre o quadrado dos números entre 10 e 150.
+PROGRAMADOR: Caio Dias (d-caio).
+DATA: 02/03/2026
+*/
+
+public class Lote1Ex31EstRep {
+    static final int INI = 10;
+    static final int FIM = 150;
+    
+    public static void main(String[] args) {
+        for (int i = INI; i <= FIM; i++) {
+            System.out.println(i + "² = " + (i * i));
+            
+        }
+    }
+}
